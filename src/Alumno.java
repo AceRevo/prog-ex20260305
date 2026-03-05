92916ecbea3729794f037d2fbf6bae99747fd27c
AceRevo <saenzleonardo8@gmail.com>
2026-03-05 08:52:58 +0100

    commit and push

---
+++ src/Alumno.java
@@ -0,0 +1,39 @@
+public class Alumno {
+
+    //Declaracion de variables sin inicializar
+    private int id;
+    private String nombre;
+    private String tituloFP;
+    private int edad;
+
+
+    //Contructo para la clase Alumno con sus atributos
+    public Alumno alumno(int id, String nombre, String tituloFP, int edad){
+
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+
+    //seccion getters
+    public int getId(){
+        return this.id;
+
+    }
+
+    public String getNombre(){
+        return  this.nombre;
+
+    }
+
+    public String getTituloFP(){
+        return  this.tituloFP;
+
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+
+}
